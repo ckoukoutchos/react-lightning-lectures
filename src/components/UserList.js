5406bcce0d41f5abd106e9092a95d31d814cecc9
Corbin Koukoutchos <ckoukoutchos@gmail.com>
2019-08-07 07:10:10 -0700

    turned users into func comp

--- src/components/UserList.js
+++ src/components/UserList.js
@@ -1,21 +1,19 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 import User from './User';
 import Spinner from './Spinner';
 import withApiCall from '../HOC';
 
-class UserList extends Component {
-  render() {
-    const { loading, users } = this.props;
+const UserList = props => {
+  const { loading, users } = props;
 
-    // creates a user card for each item in users
-    const userList = users.map(user => (
-      <User email={user.email} key={user.id} name={user.name} />
-    ));
+  // creates a user card for each item in users
+  const userList = users.map(user => (
+    <User email={user.email} key={user.id} name={user.name} />
+  ));
 
-    // displays spinner if loading, user cards otherwise
-    return loading ? <Spinner /> : userList;
-  }
+  // displays spinner if loading, user cards otherwise
+  return loading ? <Spinner /> : userList;
 }
 
 // pass app component to HOC
